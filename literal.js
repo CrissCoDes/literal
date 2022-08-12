59f203ea91b0dce293829836db79ec72d00cb805
CrissCoDes <85659901+CrissCoDes@users.noreply.github.com>
2022-08-12 11:35:47 -0400

    Add files via upload

---
+++ literal.js
@@ -0,0 +1,25 @@
+const answer = {
+    succees: ['Great!!', 'You did it', 'You Rock'],
+    failure: ['Try again', 'Not there yet', 'try harder']
+}
+
+const makeList = (arr, arr1) => {
+
+const successItems = [];
+
+for (let i = 0; i < arr.length; i++){
+    successItems.push(arr[i],)
+}
+
+const failureItems = [];
+
+for (let i = 0; i < arr1.length; i++){
+    failureItems.push(arr1[i],)
+}
+
+ console.log(successItems);
+ console.log(failureItems);
+}
+
+makelist = makeList(answer.succees, answer.failure)
+
